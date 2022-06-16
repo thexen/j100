956ed9870d86afa4f383cd6830c68457aa98a4e0
thexen <thelastundead@nate.com>
2022-06-16 21:39:35 +0900

    0616

--- temp/blockchain/dex/contracts/contracts.js
+++ temp/blockchain/dex/contracts/contracts.js
@@ -6,7 +6,7 @@
 */
 const swapHelper        = "0xc0D9a54cF6b54361Ac045cD849E8Da784a68E5f8";
 const swapPoolManager   = "0xbAa0951c51f93350771c0DD48c5D49ae5BEe8cA2";
-const swapPoolFactory   = "0xae4bA40a4Dca3Eb2E5756cAb83A16FD0694eB0C9";
+const swapPoolFactory   = "0x125e64F6594Ff13479B0d9525B9dd2FF6098552c";
 const tokenManager      = "0x42e8F7474996dA449639cb5c4125732c0a992E7a";
 
 
--- temp/blockchain/dex/klay-contract.js
+++ temp/blockchain/dex/klay-contract.js
@@ -10,6 +10,7 @@ const { DiscoveryFirstMiddleiRoute, DiscoveryRoutes, DiscoveryMiddleiRoutes } = 
 const { GetNetwork }                                            =  require ( './networks/active.js' );
 const { GetContract }                                           =  require ( './contracts/contracts.js' );
 const { SHExchange }                                            =  require ( './klaytn/swapHelper.js' );
+const { CreateSwapPool }                                        =  require ( './klaytn/spFactory.js' );
 const { InquerySHExpectedAmount }                               =  require ( './inquery/swapHelper.js' );
 const { 
         InquerySPMDailyStat,
@@ -97,23 +98,25 @@ async function main11() {
 //swap pool 자산 구하기 예제
 //LPT Staking Holder mining 보상 계산 constant 구하기 예제
 async function main02() {
-  var stat        = await InquerySPMDailyStat( "0x6a72Ffb94a5E24529fa27107297CcdccF7C95E8B", (Date.now()/1000).toFixed(0) ) ;
+  //var stat        = await InquerySPMDailyStat( "0x6a72Ffb94a5E24529fa27107297CcdccF7C95E8B", (Date.now()/1000).toFixed(0) ) ;
   //var asset       = await InquerySPAssets( "0x9A3a88729913267DEC84c3aac09499BD816f8DA4" );
-  var poolInfo    = await InquerySPPoolnfo( "0x2BEa7c33B8636a761be888E55037AFd2ee0c43c5" );
-  var miningConst = await InqueryLSHStakingRewardConst( "0x6ee6bE58DBa446dDdb75B2d979cEd3c3d6196196", "0xFf8EF2b0054Edf1A722186CE62BBE4323951e99B" );  
-//  var token1      = await InquerySPToken( "0x9A3a88729913267DEC84c3aac09499BD816f8DA4", 1 );
-//  var grade       = await InqueryTMTokenGrade( "0x21CB1A627380BAdAeF180e1346479d242aca90D3" );
-//  var token2      = await InqueryTMToken( 1 );
+  //var poolInfo    = await InquerySPPoolnfo( "0x2BEa7c33B8636a761be888E55037AFd2ee0c43c5" );
+  //var miningConst = await InqueryLSHStakingRewardConst( "0x6ee6bE58DBa446dDdb75B2d979cEd3c3d6196196", "0xFf8EF2b0054Edf1A722186CE62BBE4323951e99B" );  
+  //var token1      = await InquerySPToken( "0x9A3a88729913267DEC84c3aac09499BD816f8DA4", 1 );
+  //var grade       = await InqueryTMTokenGrade( "0x21CB1A627380BAdAeF180e1346479d242aca90D3" );
+  //var token2      = await InqueryTMToken( 1 );
+  var poolSize     = await InquerySPMPoolSize();
 
-  console.log( stat );
+  //console.log( stat );
   //console.log( asset );
-  console.log( miningConst );
-  console.log( poolInfo );
+  //console.log( miningConst );
+  //console.log( poolInfo );
   //console.log( token1 );
   //console.log( grade );
   //console.log( token2 );
+  console.log( poolSize['0'] );
 }
-main02();
+//main02();
 
 //mining 채굴량 계산하기 예제( StakingHolder s + mining fee )
 async function main03() {
@@ -160,3 +163,27 @@ async function main05() {
   console.log( obj );
 }
 //main05();
+
+//create swap pool
+async function main06() {
+
+  //var poolSize     = await InquerySPMPoolSize();
+  //console.log( poolSize['0'] );
+
+  const wallet          = await OpenWalletFromPrivateKey("0x69908c50c12b5e7aa84fe245a107431ea666ceb650b31a55c28e9bf2987d74c3");
+  //var   allowance       = await Allowance( "0xfe8c5F82DD2D74e344f94a19Be0bf1BBb59DDFdb", wallet.getAddressString(), "0x125e64F6594Ff13479B0d9525B9dd2FF6098552c" );  
+  //console.log( allowance );
+
+  //CreateSwapPool( wallet, "0xfe8c5F82DD2D74e344f94a19Be0bf1BBb59DDFdb", "0x21CB1A627380BAdAeF180e1346479d242aca90D3", 10000 );
+  
+  var poolSize     = await InquerySPMPoolSize();
+  console.log( poolSize['0'] );
+
+  var obj = await InquerySPMSPagingSwapPool( poolSize['0'], 1 );
+  console.log( obj[2][0]);
+
+  var poolInfo    = await InquerySPPoolnfo( obj[2][0] );
+  console.log( poolInfo );
+}
+
+main06();
